dfedd2ededaa99d6a5954a5839dd269925b17c9d
EDSON SANTOS <86252983+EdsonST@users.noreply.github.com>
2025-06-16 19:15:08 -0300

    Update app.js
    Adição de duas validações de segurança.

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,7 +1,28 @@
+//validação1
+if (numerode >= numeroate) {
+  alert(
+    "O número inicial deve ser menor que o número final. Por favor, corrija os valores."
+  );
+  document.getElementById("de").value = "";
+  document.getElementById("ate").value = "";
+}
+
 function sortear() {
   let quantidade = parseInt(document.getElementById("quantidade").value);
   let numerode = parseInt(document.getElementById("de").value);
   let numeroate = parseInt(document.getElementById("ate").value);
+
+//validação2
+  let quantidadeNumerosPossiveis = numeroate - numerode + 1;
+  if (quantidade > quantidadeNumerosPossiveis) {
+    alert(
+      "Não é possível sortear essa quantidade de números. Existem apenas " +
+        quantidadeNumerosPossiveis +
+        " números possíveis no intervalo escolhido."
+    );
+    reiniciar();
+    return;
+  }
 
   let numerosSorteados = [];
   let numero;
